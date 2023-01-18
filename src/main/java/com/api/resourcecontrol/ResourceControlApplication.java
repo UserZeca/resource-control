5b9432075536ac220f2ff5ebecae361018319f76
Ezequias Kluyvert de Oliveira Lemos <61207416+UserZeca@users.noreply.github.com>
2023-01-17 23:15:58 -0300

    Criando autentificação de conta com o banco de dados

--- src/main/java/com/api/resourcecontrol/ResourceControlApplication.java
+++ src/main/java/com/api/resourcecontrol/ResourceControlApplication.java
@@ -2,6 +2,7 @@ package com.api.resourcecontrol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class ResourceControlApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ResourceControlApplication.class, args);
+		System.out.println(new BCryptPasswordEncoder().encode("senha123"));
 	}
 
 }
--- src/main/java/com/api/resourcecontrol/configs/security/WebSecurityConfig.java
+++ src/main/java/com/api/resourcecontrol/configs/security/WebSecurityConfig.java
@@ -2,30 +2,15 @@ package com.api.resourcecontrol.configs.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 public class WebSecurityConfig {
-    @Bean
-    public UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER").build());
-        return manager;
-    }
+
     @Bean
     //@Order(1)
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
@@ -39,7 +24,7 @@ public class WebSecurityConfig {
         return  http.build();
     }
 
-    // @Bean
+    @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
